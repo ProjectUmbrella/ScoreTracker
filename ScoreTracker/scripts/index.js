33d4fbd39d2410d0f97098c410f05163b98b4d39
rkanubaddhi <r.kanubaddhi@gmail.com>
2015-01-03 22:06:10 -0500

    - added service for pulling data

--- ScoreTracker/scripts/index.js
+++ ScoreTracker/scripts/index.js
@@ -2,8 +2,12 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
+'use strict';
+
+
+angular.module("ScoreKeeper", ["ngRoute"]);
+
 (function () {
-    "use strict";
 
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
--- ScoreTracker/scripts/index/controller/MainController.js
+++ ScoreTracker/scripts/index/controller/MainController.js
@@ -1,15 +1,11 @@
-(function () {'use strict';
-    angular.module("ScoreKeeper", ['ngRoute']).controller('MainController', ['$scope', function($scope) {
+'use strict';
+
+
+
+	angular.module('ScoreKeeper').controller('MainController', ['$scope', 'GameService', function($scope, GameService) {
         $scope.total = {};
         $scope.score = {};
-        $scope.users = [
-            {index:1, name:'Hari', scores:[]},
-            {index:2, name:'Radhika', scores:[]},
-			{index:3, name:'Kumar', scores:[]},
-			{index:4, name:'Venky', scores:[]},
-			{index:5, name:'Harshika', scores:[]},
-			{index:6, name:'Rajesh', scores:[]}
-        ];
+        $scope.users = GameService.getData();
 	
         $scope.rounds = $scope.users[0].scores.length;
 
@@ -105,4 +101,3 @@
 		}
 	
     }]);
-})();
