055b5c56931b489f51026f922d33cc58776da98a
kumards <kumaravel@live.com>
2015-03-15 19:17:33 -0400

    1. Adding services files 2. Adding couchbase plugin

---
+++ ScoreTracker/scripts/services/data-storage.service.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+var keeperModule = angular.module("ScoreKeeper");
+keeperModule.service('dataStorageService', [function () {
+    var self = this;
+    self.getDbServerURL = function (success, fail) {
+        //cordova.exec(function (url) {
+        //    success(url);
+        //}, function (err) {
+        //    fail(err);
+        //}, "CBLite", "getURL", []);
+        return "http://someurl";
+    };
+
+    Umbrella.Service.prototype.createDbIfNotExists = function (success, fail) {
+        //Check if db exists
+
+        //Create Db If not exists
+    };
+
+    Umbrella.Service.prototype.createDb = function (success, fail) {
+
+    };
+
+    Umbrella.Service.prototype.dbExists = function (success, fail) {
+        this.getDbURL();
+    };
+}]);
